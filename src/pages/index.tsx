62acbc27c78f53f800052ddb136bc452b4a5471a
luancatarino <luancatarino@gmail.com>
2022-12-21 14:31:48 -0300

    feat: create components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
+import Layout from "../components/template/Layout";
+
 export default function Home() {
     return (
-        <div
-            className={`
-        flex justify-center items-center h-screen
-        bg-gradient-to-r from-green-500 to-blue-500
-    `}
-        >
-            <h1>Admin</h1>
-        </div>
+        <Layout title="Home Page" subtitle="Build Admin Template">
+            <h3>Content</h3>
+        </Layout>
     );
 }
